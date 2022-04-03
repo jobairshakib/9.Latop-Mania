02bc3e32d32e45eb83598c2b979375d9034dd49f
Jobair Shakib <shakib.jobair@gmail.com>
2022-04-04 05:24:51 +0600

    home section

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Navbar></Navbar>
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Home></Home>}></Route>
         <Route></Route>
         <Route></Route>
         <Route></Route>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Laptop from '../../laptop.png';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h2>THis is Home</h2>
+        <div className='home-container'>
+            <div className='text-container'>
+                <h1>WELCOME TO YOUR INITIATION</h1>
+                <span><h1>LET POWER CONSUME YOU</h1></span>
+                <p>Dual storage options and optional NVIDIA® dedicated graphics* contribute to enhanced productivity, while the integrated firmware—Trusted Platform Module (TPM) 2.0—keeps your work secure by encrypting data and passwords.</p>
+                <button>Live Demo</button>
+            </div>
+            <div className='img-container'>
+                <img src={Laptop} />
+            </div>
         </div>
     );
 };
