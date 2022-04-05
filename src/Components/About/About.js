1cf11343b2f908b64cb5ba461856b410008b8ac7
Jobair Shakib <shakib.jobair@gmail.com>
2022-04-05 17:00:04 +0600

    final commit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,9 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>This is About</h2>
+            <h2>Laptop Mania</h2>
+            <p>Copyright © 2020 Laptop Mania</p>
+            <span>All rights reserved</span>
         </div>
     );
 };
